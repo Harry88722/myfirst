171f718932a63efbb2aa048c271323fa96f87cbe
harry88722 <harriom88722@gmail.com>
2019-08-12 21:45:57 +0530

    Updates

---
+++ precache-manifest.40e60f81aa80038e5e069f4cc5eb441a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e93b3571b201e8dc2f2ca57a0d8a9a8",
+    "url": "/prectice/index.html"
+  },
+  {
+    "revision": "36153034741bfc6de99d",
+    "url": "/prectice/static/css/main.ff7bb85b.chunk.css"
+  },
+  {
+    "revision": "9bc37d27b6524ddbac94",
+    "url": "/prectice/static/js/2.d4df4b24.chunk.js"
+  },
+  {
+    "revision": "36153034741bfc6de99d",
+    "url": "/prectice/static/js/main.53569f05.chunk.js"
+  },
+  {
+    "revision": "543ade9df9dfc7c6d8ab",
+    "url": "/prectice/static/js/runtime~main.6bed3176.js"
+  }
+]);
